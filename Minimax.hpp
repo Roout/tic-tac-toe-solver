82baca85e1f31e90cd1323a2e7a5b3a9a28ac777
Roout <following2future@gmail.com>
2020-10-31 16:01:00 +0300

    feature(Minimax): add alpha-beta pruning
    close #4

--- Minimax.hpp
+++ Minimax.hpp
@@ -5,13 +5,17 @@
 
 class Minimax {
 public:
+    using Heuristic_t = float;
+
+    static constexpr Heuristic_t INFINITY { 1000000.f };
+
     /**
      * @param minimaxMark indicate which mark does algorithm use for itself ('x' by default)
      */
     Minimax(char minimaxMark);
 
     /**
-     * Run minimax algorithm with the given board state
+     * Run minimax algorithm for the given board state
      * @param board is a current game state
      * @return the best move. To extract row and col do the following:
      * - row = return_value / 3; 
@@ -19,19 +23,41 @@ public:
      */
     size_t Run(game::Board board);
 
+    /**
+     * Run minimax algorithm with alpha-beta pruning for the given board state
+     * @param board is a current game state
+     * @return the best move. To extract row and col do the following:
+     * - row = return_value / 3; 
+     * - col = return_value % 3
+     */
+    size_t RunAlphaBeta(game::Board board);
+
+    size_t ExpandedNodesCount() const noexcept;
+
 private:
 
-    float Apply(game::Board, int depth, bool isMaximizingPlayer);
+    Heuristic_t Apply(game::Board, int depth, bool isMaximizingPlayer);
+
+    Heuristic_t Apply(game::Board, int depth, Heuristic_t alpha, Heuristic_t beta, bool isMaximizingPlayer);
 
     bool IsTerminal(game::Board node) const noexcept;
 
-    float GetHeuristic(game::Board node, int depth) const noexcept;
+    Heuristic_t GetHeuristic(game::Board node, int depth) const noexcept;
 
 private:
     // character which represent AI's mark
     char m_symbol { 'x' };
     // mark of the opponent
     char m_opponent { 'o' };
+
+    // statistics:
+    // number of expanded nodes
+    size_t m_expanded { 0u };
 };
+
+inline size_t Minimax::ExpandedNodesCount() const noexcept {
+    return m_expanded;
+}
+
 
 #endif // MINIMAX_HPP
